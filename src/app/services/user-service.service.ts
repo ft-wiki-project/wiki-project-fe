75b4f7b74e2644d35dcfd9b852a79a4626be20a2
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-28 11:39:20 -0600

    feat: refactor UserService to manage user data with BehaviorSubject and localStorage

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserServiceService {
+export class UserService {
+  private userDataSubject = new BehaviorSubject<any>(null);
+  userData$ = this.userDataSubject.asObservable();
 
-  constructor() { }
+  updateUserData(data: any) {
+    localStorage.setItem('user', JSON.stringify(data));
+    this.userDataSubject.next(data);
+  }
+
+  clearUserData() {
+    localStorage.removeItem('user');
+    this.userDataSubject.next(null);
+  }
+
+  getCurrentUser() {
+    const userData = localStorage.getItem('user');
+    return userData ? JSON.parse(userData) : null;
+  }
 }
