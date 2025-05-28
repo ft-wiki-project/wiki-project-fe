3824fd05eb80740980eccafef357f2fde710c529
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-28 00:17:41 -0600

    feat: update routing configuration to include all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AnnouncementsComponent } from './announcements/announcements.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TeamsComponent } from './teams/teams.component';
+import { SelectCompanyComponent } from './select-company/select-company.component';
+import { UserRegistryComponent } from './user-registry/user-registry.component';
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
-  { path: "announcements", component: AnnouncementsComponent }
+  { path: "announcements", component: AnnouncementsComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "teams", component: TeamsComponent },
+  { path: "select-company", component: SelectCompanyComponent },
+  { path: "user-registry", component: UserRegistryComponent }
 ];
 
 @NgModule({
