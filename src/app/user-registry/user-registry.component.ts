9ecbe0b2bbf06b1a7530a5db9302707c592c8a4b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-28 00:14:38 -0600

    feat: add UserRegistryComponent with template and styles

---
+++ src/app/user-registry/user-registry.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registry',
+  standalone: false,
+  templateUrl: './user-registry.component.html',
+  styleUrl: './user-registry.component.css'
+})
+export class UserRegistryComponent {
+
+}
