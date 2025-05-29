11b08b0aaaf8e4fe48a5623a10963e7f9d2d7981
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-28 19:14:46 -0600

    feat: add isAdmin method to EditProjectModalComponent for user role validation

--- src/app/components/edit-project-modal/edit-project-modal.component.ts
+++ src/app/components/edit-project-modal/edit-project-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { UserService } from '../../services/user-service.service';
 
 @Component({
   selector: 'app-edit-project-modal',
@@ -10,6 +11,8 @@ export class EditProjectModalComponent {
   @Input() project: any;
   @Output() close = new EventEmitter<void>();
   @Output() save = new EventEmitter<any>();
+
+  constructor(private userService: UserService) {}
 
   editedProject: any = {};
 
@@ -23,5 +26,10 @@ export class EditProjectModalComponent {
 
   onClose() {
     this.close.emit();
+  }
+
+  isAdmin(): boolean {
+    const currentUser = this.userService.getCurrentUser();
+    return currentUser && currentUser.admin === "true";
   }
 }
