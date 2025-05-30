0afc7678dcbe948dd320eed1ad3a8498a27d94a8
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-29 18:42:46 -0600

    Add authGuard to route definitions for enhanced route protection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { ProjectsComponent } from './projects/projects.component';
 import { TeamsComponent } from './teams/teams.component';
 import { SelectCompanyComponent } from './select-company/select-company.component';
 import { UserRegistryComponent } from './user-registry/user-registry.component';
+import { authGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
-  { path: "announcements", component: AnnouncementsComponent },
-  { path: "projects", component: ProjectsComponent },
-  { path: "teams", component: TeamsComponent },
-  { path: "select-company", component: SelectCompanyComponent },
-  { path: "user-registry", component: UserRegistryComponent }
+  { path: "announcements", component: AnnouncementsComponent, canActivate: [authGuard] },
+  { path: "projects", component: ProjectsComponent, canActivate: [authGuard] },
+  { path: "teams", component: TeamsComponent, canActivate: [authGuard] },
+  { path: "select-company", component: SelectCompanyComponent, canActivate: [authGuard] },
+  { path: "user-registry", component: UserRegistryComponent, canActivate: [authGuard] }
 ];
 
 @NgModule({
